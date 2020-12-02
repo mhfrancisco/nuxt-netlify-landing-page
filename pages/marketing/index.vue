0c8628a56f7ee4d8637769f785f4984c4d0c84a4
Mat Francisco <mat.francisco@gamesysgroup.com>
2020-12-02 17:36:18 +0800

    Added redirect to /marketing/

--- pages/marketing/index.vue
+++ pages/marketing/index.vue
@@ -1,43 +1,9 @@
 <template>
-  <div>
-    <section class="intro">
-      <h1>Digital Acquisition Services</h1>
-    </section>
-  </div>
+  <div />
 </template>
 
-<style scoped>
-.intro {
-  height: 300px;
-  position: relative;
-  padding: 30px;
-  box-sizing: border-box;
-  background-image: url('~assets/images/banner-desktop-new.png');
-  background-position: center;
-  background-size: cover;
+<script>
+export default {
+  middelware: 'redirect'
 }
-
-.intro h1 {
-  position: absolute;
-  top: 10%;
-  left: 5%;
-  width: 90%;
-  font-size: 4.5rem;
-  color: white;
-}
-
-@media (min-width: 768px) {
-  .intro h1 {
-    font-size: 2rem;
-  }
-}
-
-.featured-posts {
-  display: flex;
-  padding: 20px;
-  box-sizing: border-box;
-  flex-wrap: wrap;
-  align-items: center;
-  justify-content: center;
-}
-</style>
+</script>
